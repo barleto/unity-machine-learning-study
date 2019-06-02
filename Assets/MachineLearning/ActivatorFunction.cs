eaa4d843bcb94601d22c1e8a88438eef67e3a3bd
Ricardo Charf <ricardo.charf@wavy.global>
2019-06-02 02:33:30 -0300

    initial commit. Perceptor done.

---
+++ Assets/MachineLearning/ActivatorFunction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ActivatorFunction
+{
+    abstract public double Calculate(double input);
+}
+
+public class StepFunction : ActivatorFunction
+{
+    public override double Calculate(double input)
+    {
+        return input < 0 ? 0 : 1;
+    }
+}
+
+public class BalancedSigmoid : ActivatorFunction
+{
+    public override double Calculate(double input)
+    {
+        double k = (double)System.Math.Exp(input);
+        return (k / (1 + k)) - 0.5f;
+    }
+}
+
+public class Tanh : BalancedSigmoid
+{
+    public override double Calculate(double input)
+    {
+        return 2 * base.Calculate(2 * input) - 1;
+    }
+}
---
+++ Assets/MachineLearning/Perceptron.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perceptron
+{
+    ActivatorFunction _activatorFunction;
+    double _bias;
+    int _inputNumber;
+    List<double> _weights = new List<double>();
+    List<double> _inputs = new List<double>();
+    double _output;
+    double _alpha;
+
+    public double Output {get{return _output;}}
+
+    public Perceptron(ActivatorFunction activatorFunction, int inputNumbers, double learningRate)
+    {
+        _activatorFunction = activatorFunction;
+        _alpha = learningRate;
+        _inputNumber = inputNumbers;
+        InitializePerceptron();
+    }
+
+    private void InitializePerceptron()
+    {
+        _bias = UnityEngine.Random.Range(-1f, 1f);
+        for (int i = 0; i < _inputNumber; i++)
+        {
+            _weights.Add(UnityEngine.Random.Range(-1f,1f));
+        }
+    }
+
+    public double Train(List<double> inputs, double desiredOutput)
+    {
+        CalcOutput(inputs);
+        AdjustWeights(inputs, desiredOutput);
+        return _output;
+    }
+
+    private double DotProductBias(List<double> inputs)
+    {
+        double res = 0;
+        for (int i = 0; i < inputs.Count; i++)
+        {
+            res += _weights[i] * inputs[i];
+        }
+        res += _bias;
+        return res;
+    }
+
+    public double CalcOutput(List<double> inputs)
+    {
+        _output = _activatorFunction.Calculate(DotProductBias(inputs));
+        return _output;
+    }
+
+    void AdjustWeights(List<double> inputs, double desiredOutput)
+    {
+        var error =  desiredOutput - _output;
+        for (int i = 0; i < _weights.Count; i++)
+        {
+            _weights[i] =inputs[i] * error + _weights[i];
+        }
+        _bias += error;
+    }
+    
+}
