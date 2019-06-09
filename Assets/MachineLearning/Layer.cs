dc242756333701a76321b0df3d58595b27a542c7
Ricardo Charf <ricardo.charf@wavy.global>
2019-06-09 17:03:54 -0300

    begin NN files structure

---
+++ Assets/MachineLearning/Layer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer
+{
+    List<Perceptron> _perceptrons = new List<Perceptron>();
+
+    public Layer(int numberOfInputs,int numberOfPerceptrons, ActivatorFunction function, double learningRate)
+    {
+        for(int i = 0; i < numberOfPerceptrons; i++)
+        {
+            _perceptrons.Add(new Perceptron(function, numberOfInputs, learningRate));
+        }
+    }
+}
---
+++ Assets/MachineLearning/NeuralNetwork.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class NeuralNetwork
+{
+    int _numberOfInputs;
+    int _numberOfOutputs;
+    List<Layer> _layers = new List<Layer>();
+    double _learningRate;
+
+    public NeuralNetwork(int numberOfInputs, int numberOfOutputs, int numberOfLayers, ActivatorFunction[] activatorsList, int[] numberOfPerceptronsInEachLayer, double learningRate = 0.01)
+    {
+        _numberOfInputs = numberOfInputs;
+        _numberOfOutputs = numberOfOutputs;
+        _learningRate = learningRate;
+        if (activatorsList.Length != numberOfLayers)
+        {
+            throw new Exception("Activators list must have same length as numberOfHiddenLayers");
+        }
+
+        if (numberOfLayers <= 0)
+        {
+            throw new Exception("Number o layers must be > 0");
+        }
+
+        int lastLayerOutputSize = numberOfInputs;
+        for(int i = 0; i < numberOfLayers; i++)
+        {
+            _layers.Add(new Layer(lastLayerOutputSize, numberOfPerceptronsInEachLayer[i], activatorsList[i], learningRate));
+            lastLayerOutputSize = numberOfPerceptronsInEachLayer[i];
+        }
+    }
+}
